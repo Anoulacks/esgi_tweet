6167339be9ff6a848219ec9f1331f29aafe1d4a5
Zinedine MEGNOUCHE <megnouche.z@gmail.com>
2023-06-30 17:33:00 +0200

    add profile_card_item

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:esgi_tweet/models/user.dart';
+import 'package:esgi_tweet/screens/search/profile_card_item.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -5,6 +8,22 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final user = UserApp(
+        firstname: "firstname",
+        lastname: "lastname",
+        pseudo: "pseudo",
+        email: "email",
+        birthDate: Timestamp.fromDate(DateTime.now()),
+        phoneNumber: "",
+        address: "address",
+        photoURL:
+            "https://v.wpimg.pl/OTMyODguYDUsGzl3SA5tIG9DbS0OV2N2OFt1ZkhEfWR9AXlzUlo8MmAYKykOGyYnOQsuJw0UYSM9GisqSQUjez4NIDYGEipmYh8qJRMAPTEpJiA2DhImOiwVYHJUQnliKB13JQRGfjd7Jn1yVEx8ZnVBYS4XEm0p");
+    return ListView.builder(
+        itemCount: 23,
+        itemBuilder: (item, index) {
+          return ProfileCardItem(
+            user: user,
+          );
+        });
   }
 }
